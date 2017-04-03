97e79b194987341e260e083419c024599d076bb2
Ryan Mahaffey <ryan.a.mahaffey@gmail.com>
2017-04-03 04:09:04 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(text)
   if text == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif text == text.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif text == text
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
