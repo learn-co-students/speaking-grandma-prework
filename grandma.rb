6f4470189943e6e0a8541ac467b6bf4a0720e47e
Ashlee Crusco <ashleecrusco@gmail.com>
2017-07-28 19:43:53 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if ("#{phrase}" != "#{phrase}".upcase)
+  if (phrase != phrase.upcase)
     "HUH?! SPEAK UP, SONNY!"
-  elsif ("#{phrase}" == "I LOVE YOU GRANDMA!")
+  elsif (phrase == "I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
   else
     "NO, NOT SINCE 1938!"
