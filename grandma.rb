0b8bde84f7d10481995e00b577b40310f61ef54c
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2017-12-10 19:48:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase.upcase != phrase
-      print "HUH?! SPEAK UP, SONNY"
+      puts "HUH?! SPEAK UP, SONNY"
   elsif phrase == "I LOVE YOU GRANDMA!"
-      print "I LOVE YOU TOO PUMPKIN!"
+      puts "I LOVE YOU TOO PUMPKIN!"
   else
-    print "NO, NOT SINCE 1938!"
+      puts "NO, NOT SINCE 1938!"
   end
 end
