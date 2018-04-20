fa4ab86f84cfce09e5af7d10a29f79b8ecb499a9
github username <github email address>
2018-04-19 19:10:35 -0700

    Edited #speak_to_grandma. spelling of elsif

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
 
   if phrase.upcase
     "NO, NOT SINCE 1938!"
-  elseif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
