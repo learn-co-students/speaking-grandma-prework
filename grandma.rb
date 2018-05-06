73c048e7f023938c7ed51c40d0004076637ecb9d
critsmet <36425627+critsmet@users.noreply.github.com>
2018-05-06 16:58:04 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 def speak_to_grandma(phrase)
-if phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1983!"
+    return "NO, NOT SINCE 1938!"
 else
-  return "I LOVE YOU TOO PUMPKIN!"
+  return "HUH?! SPEAK UP, SONNY!"
+end
 end
