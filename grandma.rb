8265d20a1bf83f4df4dc590519eb9e56143cf0b9
Laura Adam <laurawynosky@gmail.com>
2018-05-27 21:18:24 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,21 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma (phrase=="Hi Nana, how are you?")
+end
+  loud=phrase.upcase
+  soft=phrase.downcase
+  confuse=phrase.reverse
+  hug == "I LOVE YOU GRANDMA"
+ 
+if phrase==soft
+  puts "HUH?! SPEAK UP, SONNY!."
+elsif phrase == loud
+  puts "NO, NOT SINCE 1938!."
+elsif hug == "I LOVE YOU GRANDMA"
+  puts hug
+  puts "I LOVE YOU TOO PUMPKIN!"
+else
+  puts "WHATS FOR LUNCH"
+end
