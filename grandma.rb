6a522adaca58ed755ab0fbd9ae48501ac198b983
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-13 16:04:47 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,16 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(greeting)
+  
+  response = "HUH?! SPEAK UP, SONNY!"
+  
+  if greeting == greeting.upcase
+    response = "NO, NOT SINCE 1938!"
+    
+  elsif greeting == "I LOVE YOU GRANDMA!"
+    response = "I LOVE YOU TOO PUMPKIN!"
+  
+  return response 
+
+end 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
