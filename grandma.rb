d6a4a66b01ac2a77f6b3e43d9c2eca9493d58308
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-09-09 10:49:54 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (phrase)
-  if phrase == 'I LOVE YOU GRANDMA!'
-    'I LOVE YOU TOO PUMPKIN!'
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   else
