edd7a0b8a7b70ed75fa07f614d426c19b57534d3
Noah Fairbairn <noahfairbairn@gmail.com>
2018-12-08 18:36:16 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,17 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-  speak_to_grandma = gets
-end
+def speak_to_grandma(statements)
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if speak_to_grandma.upcase == speak_to_grandma
-  puts "NO, NOT SINCE 1938!"
-elsif (speak_to_grandma === "I LOVE YOU GRANDMA!")
-  puts "I LOVE YOU TOO PUMPKIN!"
+
+if ("I LOVE YOU GRANDMA!" == statements)
+   "I LOVE YOU TOO PUMPKIN!"
+elsif (statements == statements.upcase)
+   "NO, NOT SINCE 1938!"
 else
-  puts "HUH?! SPEAK UP, SONNY!"
+   "HUH?! SPEAK UP, SONNY!"
+end
 end
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
