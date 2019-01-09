08c5814ed8d5532d814c167cd29dfdae7e64e84d
Jing Cao <jingcao.yale@gmail.com>
2019-01-09 20:26:12 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (input)
   if input == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif input == input.upcase
+  elsif input == input.upcase()
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
