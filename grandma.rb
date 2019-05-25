6f47fcce27d7a34bb78635628c26edf8dbfc10d0
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-05-25 15:21:09 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,16 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
+def speak_to_grandma(phrase)
+    if phrase == 'I LOVE YOU GRANDMA!'
+        puts "I LOVE YOU TOO PUMPKIN!"
+    else if phrase == phrase.upcase
+        puts "NO, NOT SINCE 1938!"
+    else 
+        return "HUH?! SPEAK UP, SONNY!"
+    end
+end 
+
 
 
 
